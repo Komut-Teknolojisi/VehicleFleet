65e5eebe95667e50e289a2debe21b983a34112d2
Can KILIÇ <can@komutteknolojisi.com>
2020-04-02 13:08:18 +0300

    Entities

---
+++ VehicleFleet.Domain/Entities/Brand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleFleet.Domain.Entities
+{
+    public class Brand : BaseEntity
+    {
+        public string BrandName { get; set; }
+    }
+}
