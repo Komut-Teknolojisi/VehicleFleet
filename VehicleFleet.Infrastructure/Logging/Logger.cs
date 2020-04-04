a21d7298455ad1378e90860272d44e9952730c57
Can KILIÇ <can@komutteknolojisi.com>
2020-04-04 16:35:08 +0300

    Logging

---
+++ VehicleFleet.Infrastructure/Logging/Logger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+
+namespace VehicleFleet.Infrastructure.Logging
+{
+    public class Logger : ILogger
+    {
+        public IDisposable BeginScope<TState>(TState state) => null;
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public async void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            using (StreamWriter streamWriter = new StreamWriter($"{Directory.GetCurrentDirectory()}/log.txt", true))
+            {
+                await streamWriter.WriteLineAsync($"Log Level : {logLevel.ToString()} | Event ID : {eventId.Id} | Event Name : {eventId.Name} | Formatter : {formatter(state, exception)}");
+                streamWriter.Close();
+            }
+        }
+    }
+}
---
+++ VehicleFleet.Infrastructure/Logging/LoggerProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace VehicleFleet.Infrastructure.Logging
+{
+    public class LoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName) => new Logger();
+
+        public void Dispose() => throw new NotImplementedException();
+    }
+}
